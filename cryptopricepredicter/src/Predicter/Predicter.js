1caf81ad3d14b919ae51dcc53327f6d2f9ede2b5
Jan Svensson <jsvensson51@yahoo.com>
2018-09-02 14:20:56 +0200

    Install tensorflow

---
+++ cryptopricepredicter/src/Predicter/Predicter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import * as tf from '@tensorflow/tfjs';
+
+class Predicter extends Component {
+  constructor() {
+
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+}
