698225fedef2cfe1efa47aafebb06e693508a781
Patrik Elfborg <patrik.elfborg@gmail.com>
2018-10-31 22:03:28 +0100

    Added first definition of tensor flow model
    Output will only be dependant on price as input.
    
    Signed-off-by: Patrik Elfborg <patrik.elfborg@gmail.com>

--- cryptopricepredicter/src/Predicter/Predicter.js
+++ cryptopricepredicter/src/Predicter/Predicter.js
@@ -3,7 +3,12 @@ import * as tf from '@tensorflow/tfjs';
 
 class Predicter extends Component {
   constructor() {
+	const model = tf.sequential();
 
+	model.add(tf.layers.dense({units: 32, inputShape: [1]}));
+	model.add(tf.layers.dense({units: 4, kernelRegularizer: "l1l2"}));
+	model.add(tf.layers.dense({units: 1, activation: "relu"}));
+	this.model = model;
   }
 
   render() {
