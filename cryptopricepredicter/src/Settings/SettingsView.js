98d04debf946e364517773221854cffdc8677245
Jan Svensson <jsvensson51@yahoo.com>
2018-11-07 21:09:28 +0100

    Remove bootstrap from last commit

--- cryptopricepredicter/src/Settings/SettingsView.js
+++ cryptopricepredicter/src/Settings/SettingsView.js
@@ -11,20 +11,6 @@ class SettingsView extends Component {
   render() {
     return (
       <div className="Settings">
-
-        <div class="btn-group">
-          <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Action
-          </button>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
-          </div>
-        </div>
-
         <p>Settings</p>
         <h3>BTC</h3>
         <h3>ETH</h3>
