dae2d0217d95ecfb6e4e74b07ea8330d107de1e7
Jan Svensson <jsvensson51@yahoo.com>
2018-12-12 20:13:05 +0100

    Remove Choose Currency button in navbar
    The button is redundant with the button
    in the sidebar.

--- cryptopricepredicter/src/Navbar/Navbar.js
+++ cryptopricepredicter/src/Navbar/Navbar.js
@@ -5,7 +5,6 @@ class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <button>Choose currency</button>
         <div className="Navbar__title">
           <h1>Crypto Price Predicter Navbar</h1>
         </div>
