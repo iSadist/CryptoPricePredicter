c5fed58c2139028a0c2d263500bee77e081318ca
Jan Svensson <jsvensson51@yahoo.com>
2019-01-13 00:04:36 +0100

    Convert JSON data into list and make trainable
    One should be able to now run the training with
    python bitcoinPrice.py

--- NeuralNet/bitcoinPrice.py
+++ NeuralNet/bitcoinPrice.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 import json
+import numpy as np
 
 lookback_days = 90
 trained_model_file_path = "./Models/bitcoinprice.h5"
@@ -12,11 +13,21 @@ bitcoin_price_data = {}
 with open('bitcoin-price.json') as json_data:
     bitcoin_price_data = json.load(json_data)["bpi"]
 
+price_list = []
+
+for price in bitcoin_price_data.values():
+    price_list.append(price)
+
+# Putting output the same as the input
+# Needs to be changed later on
+train_correct = price_list
+train_price = price_list
+
 # Create a sequential network to look at the prices for the past days
 model = keras.Sequential([
-	    keras.layers.Dense(20, kernel_regularizer=keras.regularizers.l2(0.003), activation=tf.nn.relu, input_shape=(lookback_days, 1, 1)), # Inputs are only the price for the last lookback_days
+	    keras.layers.Dense(20, activation=tf.nn.relu, input_shape=(1,)), # Inputs are only the price for the last lookback_days
         # Add recurring layers
-	    keras.layers.Dense(20, kernel_regularizer=keras.regularizers.l2(0.003), activation=tf.nn.relu),
+	    keras.layers.Dense(20, activation=tf.nn.relu),
 	    keras.layers.Dense(1, activation=tf.nn.relu) # Only output one value, the price prediction
 	])
 
@@ -26,6 +37,9 @@ model.compile(optimizer=keras.optimizers.Adam(),
 model.summary() # Print a summary of the model
 
 # Train the model
+model.fit(train_price, train_correct, epochs=20, batch_size=10, verbose=1)
+
+print(model.predict([6500]))
 
 model.save(trained_model_file_path)
 
